76abd2c4bddd677a5a238bae2645b1a6a15ff6c5
Franro212 <105441256+Franro212@users.noreply.github.com>
2022-10-17 09:14:37 -0300

    Arreglo

--- javascript/scripts.js
+++
@@ -1,6 +0,0 @@
-const navBar = document.querySelector(".navBar")
-const botonNav = document.querySelector(".botonNav")
-
-botonNav.addEventListener("click", () =>{
-    navBar.classList.toggle("navBar_visible");
-});
