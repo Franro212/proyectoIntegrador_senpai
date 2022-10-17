8e3c87bf3c364a6b01e1b03c66430804c949c664
Franro212 <105441256+Franro212@users.noreply.github.com>
2022-10-17 09:33:22 -0300

    arreglo js

---
+++ javascript/scripts.js
@@ -0,0 +1,7 @@
+const navBar = document.querySelector(".navBar")
+const botonNav = document.querySelector(".botonNav")
+function botn (){
+    navBar.classList.toggle("navBar_visible");
+}
+botonNav.addEventListener("click", botn
+);
